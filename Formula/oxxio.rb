762a7a3772af1b127329e0c9b44b1da592005314
Syafri <syafri.login@gmail.com>
2025-10-19 00:41:36 +0800

    ok

--- Formula/oxxio.rb
+++ Formula/oxxio.rb
@@ -1,16 +1,16 @@
 class Oxxio < Formula
   desc "Oxxio CLI"
-  homepage "https://gitlab.com/oxxio	/brew-oxxio"
-  version "v0.1.2"
+  homepage "https://github.com/syafrib/brew-oxxio"
+  version "v0.1.3"
 
   on_macos do
     on_intel do
-      url "https://gitlab.com/oxxio	/brew-oxxio/-/releases/v0.1.2/downloads/oxxio_v0.1.2_darwin_amd64.tar.gz"
-      sha256 "32e0243087865c5d70bd21dfae46b853511afb037a60a6db1e413608ac1a9549"
+      url "https://github.com/syafrib/brew-oxxio/-/releases/v0.1.3/downloads/oxxio_v0.1.3_darwin_amd64.tar.gz"
+      sha256 "e4080f505e6a93f4458a73809b403996ab7c9181215d065d5bd5ca29583a6ced"
     end
     on_arm do
-      url "https://gitlab.com/oxxio	/brew-oxxio/-/releases/v0.1.2/downloads/oxxio_v0.1.2_darwin_arm64.tar.gz"
-      sha256 "a4f09d382cdf1d73416ee9b7fd811fb41cfd71306a793e812bc95f117433cc40"
+      url "https://github.com/syafrib/brew-oxxio/-/releases/v0.1.3/downloads/oxxio_v0.1.3_darwin_arm64.tar.gz"
+      sha256 "337d6d29baec66b27d8c95fa0f1f62036e53b1a8ca4e4f2badd08c1bf5a18339"
     end
   end
 

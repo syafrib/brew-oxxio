0cd6e8ab01551056424117b461e5c8ea5f212135
Syafri <syafri.login@gmail.com>
2025-10-19 01:33:49 +0800

    Update oxxio formula v0.1.5

--- Formula/oxxio.rb
+++ Formula/oxxio.rb
@@ -1,16 +1,16 @@
 class Oxxio < Formula
   desc "Oxxio CLI"
   homepage "https://github.com/syafrib/brew-oxxio"
-  version "v0.1.4"
+  version "v0.1.5"
 
   on_macos do
     on_intel do
-      url "https://github.com/syafrib/brew-oxxio/releases/download/v0.1.4/oxxio_v0.1.4_darwin_amd64.tar.gz"
-      sha256 "399623401beb4a3168d1bc41acd5ec20b9d1e09e50764fe40b1d9b64b51c4f6b"
+      url "https://github.com/syafrib/brew-oxxio/releases/download/v0.1.5/oxxio_v0.1.5_darwin_amd64.tar.gz"
+      sha256 "4e6e3daed9503038fe00530b80fec88d826bed4e9382500d47c5be8e6cb8de74"
     end
     on_arm do
-      url "https://github.com/syafrib/brew-oxxio/releases/download/v0.1.4/oxxio_v0.1.4_darwin_arm64.tar.gz"
-      sha256 "e60d262dd75a27867fe410f234ee6755bd96705b88bb764dee06aebce28ca867"
+      url "https://github.com/syafrib/brew-oxxio/releases/download/v0.1.5/oxxio_v0.1.5_darwin_arm64.tar.gz"
+      sha256 "1a7794659bbfe17355e19eff0563353af987aeb4f7ce8b485022410a8c94de58"
     end
   end
 
